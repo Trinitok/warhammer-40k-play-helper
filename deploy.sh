3e1715b5b4654ee791b54ecc671c310acbfc534f
Max <max@Maxs-MacBook-Pro.local>
2023-07-31 18:23:38 -0500

    fixed deployment to gh pages

--- deploy.sh
+++ deploy.sh
@@ -16,10 +16,12 @@ git init
 git add -A
 git commit -m 'deploy'
 
+git remote add origin git@github.com:Trinitok/warhammer-40k-play-helper.git
+
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f origin master
 
 cd -
